fc6427300903c001e85ef283c6ef7552b66c7be0
vamsichinnu <vamsichinnu@missionrnd.com>
2016-03-15 01:36:53 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -12,12 +12,68 @@ NOTES:
 */
 
 #include <stdio.h>
-
+#include<stdlib.h>
 struct node {
 	int num;
 	struct node *next;
 };
 
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+	struct node *temp, *temp1, *res;
+	if (head2&&head1)
+	{
+		res = (struct node *)malloc(sizeof(struct node) * 1);
+		temp = (struct node *)malloc(sizeof(struct node) * 1);
+		temp1 = (struct node *)malloc(sizeof(struct node) * 1);
+		if (head1->num > head2->num)
+		{
+			temp = head1;
+			head1 = head2;
+			head2 = temp;
+		}
+		res = head1;
+		while (head2 || head1)
+		{
+
+			if (head1&&head2)
+			{
+				if (head1->num <= head2->num)
+				{
+
+					temp = head1;
+					head1 = head1->next;
+				}
+				else
+				{
+					temp1 = head2->next;
+					head2->next = temp->next;
+					temp->next = head2;
+					head2 = temp1;
+					temp = temp->next;
+				}
+			}
+			else if (head1 == NULL&&head2)
+			{
+				temp->next = head2;
+				break;
+			}
+			else if (head2 == NULL&&head1)
+			{
+				break;
+			}
+		}
+		return res;
+	}
+	else if (head1 == NULL&&head2)
+	{
+		return head2;
+	}
+	else if (head2 == NULL&&head1)
+	{
+		return head1;
+	}
+	else
+	{
+		return NULL;
+	}
 }
